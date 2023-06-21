fdbfb3d90f01d1e6b29953cb39b56795ea94cf45
PhillUndSoos <ph_hass@web.de>
2023-06-21 19:20:15 +0200

    Get a Random Movie
    sends a request to the TMDB API with the selected genre as paramater, returning a list of movies of selected genre

--- public/script.js
+++ public/script.js
@@ -5,25 +5,40 @@ const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
-  const genreRequestEndpoint = '/genre/movie/list'
-  const requestParams = `?api_key=${tmdbKey}`
-  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
   try {
-    const response = await fetch(urlToFetch)
+    const response = await fetch(urlToFetch);
     if (response.ok) {
-      const jsonResponse = await response.json()
-      const genres = jsonResponse.genres
-      return genres
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
     }
   } catch(error) {
     console.log(error)
-  }
+  };
 };
 
-const getMovies = () => {
+const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres${selectedGenre}`;
+  urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results
+      return movies
+    }
+  } catch (error) {
+    console.log(error)
+  };
 
 };
+getMovies()
+
 
 const getMovieInfo = () => {
 
