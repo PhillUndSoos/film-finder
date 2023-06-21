b2057ee8919e5dd2b13317779e95cd9df7e2305b
PhillUndSoos <ph_hass@web.de>
2023-06-21 18:30:19 +0200

    Dropdown menu now populated with genres
    Sends a GET request to the TMDB, requesting a json contanig an array of objects of all genres

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 //this is the file i'll mainly be working on. Most of style.css, helpers.js, index.html aswell as the file structure were provided by Codecademy.com
 
-const tmdbKey = '';
-const tmdbBaseUrl = '';
+const tmdbKey = '0d91321457464055b1b02750ce6dcc2c';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
-
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list'
+  const requestParams = `?api_key=${tmdbKey}`
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      const genres = jsonResponse.genres
+      return genres
+    }
+  } catch(error) {
+    console.log(error)
+  }
 };
 
 const getMovies = () => {
